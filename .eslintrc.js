fcadc423ee99a8fcd4f4290d00972d98956e6ef8
罗国雄 <1025558554@qq.com>
2021-07-27 17:00:42 +0800

    feat: init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
-  'extends': ['@tools-list/eslint-config-self'],
-  'rules': {
+  extends: ['@tools-list/eslint-config-self'],
+  rules: {
     'import/no-default-export': 0,
     'react/prop-types': 0,
+    'react/display-name': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
   },
 };
