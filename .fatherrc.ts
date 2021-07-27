f0ad8a21460b90b6fe137e0010c36f8aa7fff7d0
罗国雄 <1025558554@qq.com>
2021-07-27 16:45:05 +0800

    feat: init

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,7 +1,6 @@
 export default {
   //   esm: 'rollup',
   //   cjs: 'rollup',
-  publicPath: '/build-your-lib',
   esm: { type: 'babel' },
   cjs: { type: 'babel' },
   pkgs: ['hooks', 'components', 'utils'],
