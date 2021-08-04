4fd930f51f0cf79b94fd1d53e5b88affb36071a2
huangpeng <huangpeng@codemao.cn>
2021-08-04 16:53:14 +0800

    feat: 系统cookie操作方法

--- packages/utils/src/cookie/__test__/index.test.ts
+++ packages/utils/src/cookie/__test__/index.test.ts
@@ -1,13 +1,63 @@
-import { DccCookie } from '../index';
+import { COOKIE_COMMON_KEY, dccCookie, COOKIE_TRANS_KEY } from '../index';
 
 
-describe('DccCookie test', () => {
+describe('dccCookie test', () => {
   it('common cookie set', () => {
-    DccCookie.setCookie('a', '1');
-    expect(DccCookie.getCookie('a')).toBe('1');
+    dccCookie.setCookie(COOKIE_COMMON_KEY.IS_SAAS, 102);
+    expect(dccCookie.getCookie(COOKIE_COMMON_KEY.IS_SAAS)).toBe('102');
   });
   it('obj cookie set', () => {
-    DccCookie.setObjCookie('a', { a: 1 });
-    expect(DccCookie.getObjCookie('a')).toEqual({ a: 1 });
+    dccCookie.setTransCookie(COOKIE_TRANS_KEY.USER_INFO, { a: 1 });
+    expect(dccCookie.getTransCookie(COOKIE_TRANS_KEY.USER_INFO)).toEqual({ a: 1 });
+  });
+  it('obj cookie remove', () => {
+    dccCookie.setTransCookie(COOKIE_TRANS_KEY.USER_INFO, { a: 1 });
+    dccCookie.removeCookie(COOKIE_TRANS_KEY.USER_INFO);
+    expect(dccCookie.getTransCookie(COOKIE_TRANS_KEY.USER_INFO)).toBeFalsy();
+  });
+  it('common cookie remove', () => {
+    dccCookie.setCookie(COOKIE_COMMON_KEY.IS_SAAS, 1);
+    dccCookie.removeCookie(COOKIE_COMMON_KEY.IS_SAAS);
+    expect(dccCookie.getTransCookie(COOKIE_TRANS_KEY.USER_INFO)).toBeFalsy();
+  });
+  it('cookie secure ', () => {
+    dccCookie.setCookie(COOKIE_COMMON_KEY.IS_SAAS, 12);
+    expect(dccCookie.getCookie(COOKIE_COMMON_KEY.IS_SAAS)).toBe('12');
+    dccCookie.setCookie(COOKIE_COMMON_KEY.IS_SAAS, 123, { secure: true });
+    expect(dccCookie.removeCookie(COOKIE_COMMON_KEY.IS_SAAS)).not.toBe('123');
+  });
+  it('cookie secure expires', () => {
+    dccCookie.setCookie(COOKIE_COMMON_KEY.IS_SAAS, 12);
+    expect(dccCookie.getCookie(COOKIE_COMMON_KEY.IS_SAAS)).toBe('12');
+    dccCookie.setCookie(COOKIE_COMMON_KEY.IS_SAAS, 12, { expires: new Date(1970) });
+    expect(dccCookie.getCookie(COOKIE_COMMON_KEY.IS_SAAS)).not.toBe('12');
+    dccCookie.setCookie(COOKIE_COMMON_KEY.IS_SAAS, 12, { expires: Infinity });
+    expect(dccCookie.getCookie(COOKIE_COMMON_KEY.IS_SAAS)).toBe('12');
+  });
+  it('cookie domain', () => {
+    dccCookie.setCookie(COOKIE_COMMON_KEY.IS_SAAS, 11);
+    dccCookie.setCookie(COOKIE_COMMON_KEY.IS_SAAS, 12, { domain: 'codemao.biz' });
+    expect(dccCookie.getCookie(COOKIE_COMMON_KEY.IS_SAAS)).not.toBe('12');
+    dccCookie.setCookie(COOKIE_COMMON_KEY.IS_SAAS, 12, { domain: 'localhost' });
+    expect(dccCookie.getCookie(COOKIE_COMMON_KEY.IS_SAAS)).toBe('12');
+  });
+  it('setCookie key undefined', (done) => {
+    try {
+      expect(dccCookie.setCookie(undefined, 11));
+    } catch (error) {
+      done();
+    }
+  });
+  it('setTransCookie key undefined', (done) => {
+    try {
+      expect(dccCookie.setTransCookie(undefined, 11));
+    } catch (error) {
+      done();
+    }
+  });
+  it('cookie key ', () => {
+    expect(COOKIE_TRANS_KEY.USER_INFO).toBe('userInfo');
+    expect(COOKIE_COMMON_KEY.AUTHORIZATION).toBe('authorization');
+    expect(COOKIE_COMMON_KEY.IS_SAAS).toBe('isSaas');
   });
 });
--- packages/utils/src/cookie/index.ts
+++ packages/utils/src/cookie/index.ts
@@ -1,15 +1,63 @@
+
+/**
+ * 公共KEY无前缀
+ * 端口私有加对应端口前缀 school、student、curr、manager...
+ */
 // --需要转化key
 export enum COOKIE_TRANS_KEY {
-  IS_SAAS = 'isSaas'
+  USER_INFO = 'userInfo', // 用户相关信息
 }
+// --普通key
 export enum COOKIE_COMMON_KEY {
-  AUTHORIZATION = 'authorization' // 登录token
+  AUTHORIZATION = 'authorization', // 登录token
+  IS_SAAS = 'isSaas', //  标示当前用户所属机构类型
 }
-export const DccCookie = {
-  // 序列化cookie
-  serializeCookie: (name:string, val:string) => `${encodeURIComponent(name)}=${encodeURIComponent(val)}`,
-  // 格式化cookie
-  parseCookie() {
+interface ICookieOptions {
+  path?:string;
+  domain?:string;
+  secure?:boolean;
+  expires?:number | string | Date;
+}
+class DccCookie {
+  constructor() {
+    this.subDomain = this.getSubDomain();
+  }
+  subDomain = '' // 当前二级
+  /**
+ * 获取当前 URL 二级域名
+ * 如果当前是 IP 地址，则直接返回 IP Address
+ */
+  getSubDomain = () => {
+    try {
+      let subdomain = '';
+      const { domain } = document;
+      const domainList = domain.split('.');
+      const ipAddressReg = /^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$/;
+      // 若为 IP 地址、localhost，则直接返回
+      if (ipAddressReg.test(domain) || domain === 'localhost') {
+        return domain;
+      }
+      const urlItems = [];
+      urlItems.unshift(domainList.pop());
+      urlItems.unshift(domainList.pop());
+      subdomain = urlItems.join('.');
+      return subdomain || document.domain;
+    } catch (e) {
+      return document.domain || 'localhost';
+    }
+  }
+
+  /**
+   * cookie-key-value转码
+   * @param name
+   * @param val
+   */
+  serializeCookie = (name:COOKIE_COMMON_KEY | COOKIE_TRANS_KEY, val:any) => `${encodeURIComponent(name)}=${encodeURIComponent(val)}`
+  /**
+   * 将cookie转为对象
+   * @returns obj
+   */
+  parseCookie = () => {
     const str = document.cookie;
     try {
       return str.split(';').map((v) => v.split('=')).reduce((acc, v) => {
@@ -19,41 +67,96 @@ export const DccCookie = {
     } catch (error) {
       return {};
     }
-  },
-  // 设置cookie
-  setCookie(key:string, value:any, options?) {
+  }
+  /**
+   * 设置cookie-domain\path\expires\secure
+   * @param options:ICookieOptions
+   */
+  setOptions = (options:ICookieOptions) => {
+    let cookieOptions = '';
+    Object.keys(options).forEach((Ckey) => {
+      const item = options[Ckey];
+      if (Ckey === 'expires') {
+        switch (item.constructor) {
+          case Number:
+            cookieOptions += item === Infinity ? '; expires=Fri, 31 Dec 9999 23:59:59 GMT' : `; max-age=${item}`;
+            break;
+          case String:
+            cookieOptions += `; expires=${item}`;
+            break;
+          case Date:
+            cookieOptions += `; expires=${item.toUTCString()}`;
+            break;
+        }
+      } else if (Ckey === 'secure' && item) {
+        cookieOptions += `; ${Ckey}`;
+      } else {
+        cookieOptions += `; ${Ckey}=${item}`;
+      }
+    });
+    // 设置cookie域
+    if (!cookieOptions.includes('domain')) {
+      cookieOptions += `; domain=${this.subDomain}`;
+    }
+    return cookieOptions;
+  }
+  /**
+   * 设置普通cookie
+   * @param key
+   * @param value
+   * @param options
+   */
+  setCookie = (key:COOKIE_COMMON_KEY, value:any, options?:ICookieOptions) => {
     if (!key) {
       throw new Error('key is required');
     }
     let cookieVal = this.serializeCookie(key, value);
-    if (options) {
-      Object.keys(options).forEach((item) => {
-        cookieVal += `;${item}=${options[item]}`;
-      });
-    }
+    cookieVal += this.setOptions(options || {});
     document.cookie = cookieVal;
-  },
-  // 设置需cookie
-  setTransCookie(key:string, value:any, options?) {
+  }
+  /**
+   * 设置像一些对象、数值等cookie
+   * @param key
+   * @param value
+   * @param options
+   */
+  setTransCookie = (key:COOKIE_TRANS_KEY, value:any, options?:ICookieOptions) => {
     if (!key) {
       throw new Error('key is required');
     }
     let cookieVal = this.serializeCookie(key, JSON.stringify(value));
-    if (options) {
-      Object.keys(options).forEach((item) => {
-        cookieVal += `;${item}=${options[item]}`;
-      });
-    }
+    // 设置配置项
+    cookieVal += this.setOptions(options || {});
     document.cookie = cookieVal;
-  },
-  // 获取cookie
-  getCookie(key) {
-    return this.parseCookie()[key] || '';
-  },
-  // 获取需解析Cookie
-  getTransCookie(key) {
+  }
+  /**
+   * 获取普通cookie
+   * @param key
+   */
+  getCookie = (key) => this.parseCookie()[key] || ''
+  /**
+   * 获取需转化cookie
+   * @param key
+   * @returns
+   */
+  getTransCookie = (key:COOKIE_TRANS_KEY) => {
     const cookieVal = this.getCookie(key);
     return cookieVal ? JSON.parse(cookieVal) : '';
-  },
-};
+  }
 
+  /**
+   * 清除cookie
+   * @param key
+   * @returns
+   */
+  removeCookie = (key:COOKIE_TRANS_KEY|COOKIE_COMMON_KEY, options?:ICookieOptions) => {
+    if (!key) return false;
+    this.setCookie(key as COOKIE_COMMON_KEY, '', {
+      ...options,
+      expires: 'Thu, 01 Jan 1970 00:00:00 GMT',
+    });
+    return true;
+  }
+}
+
+export const dccCookie = new DccCookie();
