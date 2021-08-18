162795ac4dd4836684f33f0e858dbf8fd1477ea0
罗国雄 <1025558554@qq.com>
2021-08-18 16:55:39 +0800

    feat: 添加ali上传组件

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,3 +1,4 @@
 export { default as add } from './add';
 export { COOKIE_COMMON_KEY, COOKIE_TRANS_KEY, dccCookie } from './cookie';
 export { default as loadResource } from './loadResource';
+export { default as aliUpload } from './aliUpload';
