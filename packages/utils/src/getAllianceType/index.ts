8637d661aeba9c6e37506a7624470a9e1944823b
Ryan Xue <axlroseart@163.com>
2021-09-16 21:45:57 +0800

    feat: 跳转方法优化

---
+++ packages/utils/src/getAllianceType/index.ts
@@ -0,0 +1,14 @@
+// 获取当前访问域名跟code的对应关系
+export const getAllianceType = function(){
+  const { origin } = location;
+  // 学大
+  if(origin.includes('xueda')){
+    return '103';
+  }
+  // 谷雨
+  if(origin.includes('guyu')){
+    return '101';
+  }
+  // 标准版点程云
+  return '102';
+};
--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -2,3 +2,5 @@ export { default as add } from './add';
 export { COOKIE_COMMON_KEY, COOKIE_TRANS_KEY, dccCookie } from './cookie';
 export { default as loadResource } from './loadResource';
 export { default as aliUpload } from './aliUpload';
+export { DccRedirect } from './redirect';
+export { getAllianceType } from './getAllianceType';
